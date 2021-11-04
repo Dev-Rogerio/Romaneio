57f25dfdb3143bec78820a8a7d9d80196040745f
Dev-Rogerio <roger.ngt3494@gmail.com>
2021-11-04 11:17:51 -0300

    romaneio

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Header from './components/Header/app.jsx'
+import './index.css';
+
+
+export default function App () {
+    return (
+        <>
+        <p className='Deus_Fiel'>Deus é Fiél</p>
+        <Header />
+        
+
+
+        </>
+    );
+}
---
+++ components/Header/app.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+import './styled.css';
+
+
+export default function App() {
+
+
+    const [cad, setCad] = useState('')
+
+    const msglogin = () => {
+        return 'Boa Noite'
+    }
+
+    const msglogoff = () => {
+        return 'Prezado Cliente'
+     }
+    const msglogo = () => {
+        return ' é um Prazer Receber Você.'
+    }
+    const msgvazio = () => {
+        return ''
+    }
+
+    
+    return (
+
+        <div className='containerfirst'>
+            <label className='cadCli'>Informe seu nome aqui:</label>
+            <input className='cadCli_box' type='text' size='37' maxLength='40' placeholder='Nome' value={cad} onChange={(e) => { setCad(e.target.value) }}></input>
+        
+            <p className="res_name"> {cad?msglogoff():msglogin()} {cad}{cad?msglogo() : msgvazio()}</p>
+
+         
+        
+        
+        
+        </div>
+
+
+    )
+}
